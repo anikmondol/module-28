9209ef53fc7879ad4ccae828f7e819f88169fb1b
anikmondol <anikmondol558363@gmil.com>
2023-09-08 21:05:25 +0600

    my first commit

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -1,36 +1,71 @@
 function calculateTriangleArea(){
-    // get triangle base value
-    const baseField = document.getElementById('triangle-base');
-    const base = parseFloat(baseField.value);
-    
+    const base = getInputValue('triangle-base');
+    const height = getInputValue('triangle-height');
 
-    // get triangle height value
-    const heightField = document.getElementById('triangle-height');
-    const height = parseFloat(heightField.value);
-
-    // calculate area
     const area = 0.5 * base * height;
-    
 
-    // show triangle area
-    const triangleAreaSpan = document.getElementById('triangle-area');
-    triangleAreaSpan.innerText = area;
+    setElementInnerText('triangle-area',area);
 }
 
 function calculateRectangleArea(){
-    // get rectangle width value
-    const widthField = document.getElementById('rectangle-width');
-    const width = parseFloat(widthField.value);
-
-
-    // get rectangle length vale
-    const lengthField = document.getElementById('rectangle-length');
-    const length = parseFloat(lengthField.value);
-
-    // calculate area
+    const width = getInputValue('rectangle-width');
+    const length = getInputValue('rectangle-length');
     const area = width * length;
+    
+    setElementInnerText('rectangle-area',area)
+    
+}
 
-    // show rectangle area 
-    const rectangleAreaSpan = document.getElementById('rectangle-area');
-    rectangleAreaSpan.innerText = area;
+
+function calculateParallelogramArea(){
+    const base = getInputValue('parallelogram-base');
+    const height = getInputValue('parallelogram-height');
+
+    const area = base * height
+
+    setElementInnerText('parallelogram-area',area);
+}
+
+function calculateEllipsesArea(){
+    const majorRedis = getInputValue('ellipses-major-radiuses');
+    const minorRedis = getInputValue ('ellipses-minor-radiuses');
+
+    const area = 3.1416 * majorRedis * minorRedis;
+    const areaDecimal = area.toFixed(2);
+
+    setElementInnerText('ellipse-area',areaDecimal);
+}
+
+function calculateRhombusArea(){
+    const big = getInputValue('rhombus-major-base');
+    const small  = getInputValue('rhombus-minor-base');
+    area = 0.5 * big * small;
+    setElementInnerText('rhombus-area',area);
+}
+
+function calculatePentagonArea(){
+    const perimeter = getInputValue('pentagon-perimeter');
+    const apothem = getInputValue('pentagon-apothem');
+    const area = 0.5 * perimeter * apothem;
+    setElementInnerText('pentagon-area',area);
+}
+
+
+// reusable get input value field in number
+function getInputValue(fieldId){
+    const inputField = document.getElementById(fieldId);
+    const value = parseFloat(inputField.value);
+    inputField.value = '';
+    return value;
+}
+
+
+
+
+
+// reuseable set span, p, div, etc text
+
+function setElementInnerText(elementId,allArea){
+    const element = document.getElementById(elementId);
+    element.innerText = allArea;
 }
